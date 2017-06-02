2f61295f33cd022ffab0bc90764b00ef59cb7bed
Boekemeyer <tboekemeyer@hotmail.de>
2017-06-02 12:00:08 +0200

    added spawn mechanic + sounds

---
+++ Unity Projekt/Slam Drunk Inn/Assets/_Scripts/GlasSpawn.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlasSpawn : MonoBehaviour {
+
+    public Transform GlassPrefab;
+    float timer = 0f;
+
+    void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        timer += Time.deltaTime;
+        if(timer >= 1)
+        {
+            Instantiate(GlassPrefab, transform.position, Quaternion.identity);
+            timer = 0f;
+        }
+    }
+}
---
+++ Unity Projekt/Slam Drunk Inn/Assets/_Scripts/GlassMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlassMovement : MonoBehaviour {
+
+    float speed = 7.5f;
+
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+}
